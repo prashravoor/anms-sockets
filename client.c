d480b51d46afecccbade4d230c760c273a008a8f
Prashanth Ravoor <prash.ravoor125@gmail.com>
2018-10-16 12:06:46 -0700

    Better error handling, and configurable port and queue size for server

--- client.c
+++ client.c
@@ -12,12 +12,17 @@
 #define SOCKET_DOMAIN AF_INET
 #define SOCKET_TYPE SOCK_STREAM
 #define MAX 1000
+#define MAX_RETRIES 3
+#define SLEEP_INTERVAL_MICROS 100000 // 100ms
 
 extern int errno;
 
-void error(char *msg)
+void error(char *msg, int socket)
 {
     perror(msg);
+    if(socket > 0) {
+        close(socket);
+    }
     exit(1);
 }
 
@@ -25,44 +30,75 @@ int main(int argc, char **argv)
 {
     int client_socket, port = DEFAULT_SERVER_PORT;
     struct sockaddr_in server_address;
-    int numBytesWritten, numBytesRead;
-    char buffer[MAX];
+    int numBytesRead, retryCount = 0, connectCode = -1;
+    char buffer[MAX], *serverIp = NULL;
+
+    if(argc != 2 && argc != 4) {
+        printf("Invalid arguments\n\nUsage: %s <filename> <Optional Server IP> <Optional Server port>\n", argv[0]);
+        return 1;
+    }
+
+    if(4 == argc) {
+        serverIp = argv[2];
+        port = atoi(argv[3]);
+        if(port < 1024) {
+            printf("Port number is not valid, specify a port value >= 1024\n");
+            return 1;
+        }
+    }
+
+    if(strnlen(argv[1], MAX) >= MAX) {
+        printf("The filename provided is too long\n");
+        return 1;
+    }
 
     client_socket = socket(SOCKET_DOMAIN, SOCKET_TYPE, 0);
     if (client_socket < 0)
     {
-        error("Failed to create the socket for the client");
+        error("Failed to create the socket for the client", client_socket);
     }
     printf("Succesfully created socket: %d\n", client_socket);
+
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = SOCKET_DOMAIN;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(port);
 
     printf("Attempting to connect to server on port %d\n", port);
-    if (connect(client_socket, (const struct sockaddr *)&server_address, sizeof(server_address)) < 0)
-    {
-        close(client_socket);
-        error("Failed to connect to the server");
+    while (retryCount++ < MAX_RETRIES && connectCode < 0) {
+        connectCode = connect(client_socket, (const struct sockaddr *)&server_address, sizeof(server_address));
+        if(0 == connectCode || errno != ETIMEDOUT) {
+            break;
+        }
+        perror("Failed to connect to the server");
+        printf("Retrying after %d ms\n", (SLEEP_INTERVAL_MICROS/1000));
+        usleep(SLEEP_INTERVAL_MICROS);
     }
+    if(connectCode < 0) {
+        error("Failed to connect to the server", client_socket);
+    }
+
     printf("Successfully connected to server, Sending the File name\n");
     memset(buffer, 0, sizeof(buffer));
     strncpy(buffer, argv[1], strnlen(argv[1], MAX));
+
     if (send(client_socket, buffer, strnlen(argv[1], MAX), 0) < 0)
     {
-        close(client_socket);
-        error("Failed to write the data to the server");
+        error("Failed to write the data to the server", client_socket);
     }
 
     memset(buffer, 0, sizeof(buffer));
     printf("Attempting to read contents of file\n");
-    if (recv(client_socket, buffer, MAX, 0) < 0)
+    while ((numBytesRead = recv(client_socket, buffer, MAX, 0)) > 0)
     {
-        close(client_socket);
-        error("Failed to read file contents");
+        printf("%s", buffer);
+        memset(buffer, 0, sizeof(buffer));
     }
 
-    printf("Received: %s\n", buffer);
+    printf("\n");
+    if(numBytesRead < 0) {
+        error("Failed to read contents of the file", client_socket);
+    }
 
     close(client_socket);
     return 0;
--- server.c
+++ server.c
@@ -6,8 +6,10 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 
 #define DEFAULT_SERVER_PORT 8080
+#define DEFAULT_QUEUE_SIZE 5
 #define SOCKET_DOMAIN AF_INET
 #define SOCKET_TYPE SOCK_STREAM
 #define MAX 1000
@@ -15,23 +17,54 @@
 extern int errno;
 
 void serveFile(int server_socket, int client_socket);
+int openFileForRead(char *name);
 
-void error(char *msg)
+void error(char *msg, int socket)
 {
     perror(msg);
+    if (socket > 0)
+    {
+        close(socket);
+    }
     exit(1);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    int server_socket, client_socket, port = DEFAULT_SERVER_PORT;
+    int server_socket, client_socket, port = DEFAULT_SERVER_PORT, maxQueueSize = DEFAULT_QUEUE_SIZE;
     struct sockaddr_in server_address, client_address;
+    char *ipAddress = NULL;
     socklen_t client_len = 0;
+
+    // Usage: rcatserver <Optional IP> <Optional Port> <Optional Max Queue Size>
+    if (argc != 1 && argc != 4)
+    {
+        printf("Invalid command syntax\n\nUsage: %s <Optional IP> <Optional Port> <Optional Max Queue Size>\n", argv[0]);
+        return 1;
+    }
+
+    if (4 == argc)
+    {
+        ipAddress = argv[1];
+        port = atoi(argv[2]);
+        if (port < 1024)
+        {
+            printf("Port number is not valid, specify a port value >= 1024\n");
+            return 1;
+        }
+
+        maxQueueSize = atoi(argv[3]);
+        if (maxQueueSize < 0 || maxQueueSize > 5)
+        {
+            printf("Invalid value for max queue size, enter a value between 0 and 5\n");
+            return 1;
+        }
+    }
 
     server_socket = socket(SOCKET_DOMAIN, SOCKET_TYPE, 0);
     if (server_socket < 0)
     {
-        error("Failed to create the socket for the server");
+        error("Failed to create the socket for the server", server_socket);
     }
     printf("Succesfully created socket: %d\n", server_socket);
 
@@ -41,18 +74,27 @@ int main()
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(port);
 
-    if (bind(server_socket, (const struct sockaddr*)&server_address, sizeof(server_address)))
+    if (bind(server_socket, (const struct sockaddr *)&server_address, sizeof(server_address)))
     {
-        error("Failed to bind the socket to the server");
+        error("Failed to bind the socket to the server", server_socket);
     }
+
     printf("The server is bound to port %d\n", port);
-    while(1) {
-        if(listen(server_socket, 0)) {
-            error("Listening for connections interrupted");
-        }
+    if (listen(server_socket, maxQueueSize))
+    {
+        error("Listening for connections interrupted", server_socket);
+    }
+
+    while (1)
+    {
         printf("Server is now listening for connections\n");
-        client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_len);
+        client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_len);
         printf("Connection received: %d\n", client_socket);
+        if (client_socket < 0)
+        {
+            perror("Failed to accept the connection");
+            continue;
+        }
         serveFile(server_socket, client_socket);
         close(client_socket);
     }
@@ -61,29 +103,54 @@ int main()
     return 0;
 }
 
-void serveFile(int server_socket, int client_socket) {
-    char buffer[MAX];
-    int numBytesRead = 0;
+void serveFile(int server_socket, int client_socket)
+{
+    char buffer[MAX], *error = NULL;
+    int numBytesRead = 0, fd = 0;
     printf("Attempting to read file name from socket %d\n", client_socket);
     memset(buffer, 0, sizeof(buffer));
 
     numBytesRead = recv(client_socket, buffer, MAX, 0);
-    if(numBytesRead < 0) {
+    if (numBytesRead < 0)
+    {
         perror("Failed to read the file name from the client");
-        close(client_socket);
-        //close(server_socket);
         return;
     }
 
     printf("Attempting to read file %s\n", buffer);
-    printf("Writing file to client\n");
-    if(send(client_socket, buffer, numBytesRead, 0) < 0 ) {
-        perror("Failed to send the data to the client");
-    } else {
-        printf("Successfully served client %d\n", client_socket);
+    fd = openFileForRead(buffer);
+    memset(buffer, 0, sizeof(buffer));
+    if (fd < 0)
+    {
+        perror("Failed to open the file");
+        strncpy(buffer, strerror(errno), MAX);
+        if (send(client_socket, buffer, strlen(strerror(errno)), 0) < 0)
+        {
+            perror("Failed to send the data to the client");
+        }
+        return;
     }
 
-    close(client_socket);
-    //close(server_socket);
+    printf("Writing file to client\n");
+    while((numBytesRead = read(fd, buffer, sizeof(buffer))) > 0) {
+        if (send(client_socket, buffer, numBytesRead, 0) < 0)
+        {
+            perror("Failed to send the data to the client");
+            break;
+        }
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    close(fd);
     return;
+}
+
+int openFileForRead(char *filename)
+{
+    if (NULL == filename)
+    {
+        return -1;
+    }
+
+    return open(filename, O_RDONLY);
 }
